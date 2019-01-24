a0d0d8f0c78bafe9da3849d7259048e3b499c1b2
rameshsoft1 <38454779+rameshsoft1@users.noreply.github.com>
2019-01-24 23:23:43 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Register from './auth/Register';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <header className="App-header row"> 
+          <div className="col">Header</div>
+        </header>
+        <Register></Register>
+      </div>
+    );
+  }
+}
+
+export default App;
