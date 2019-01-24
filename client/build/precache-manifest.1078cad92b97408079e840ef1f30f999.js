254102506375d4e2ddf6142dec3824bab294fbb4
rameshsoft1 <38454779+rameshsoft1@users.noreply.github.com>
2019-01-24 23:19:19 +0530

    Add files via upload

---
+++ client/build/precache-manifest.1078cad92b97408079e840ef1f30f999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "970aab89c3a006e0a964",
+    "url": "/static/js/main.970aab89.chunk.js"
+  },
+  {
+    "revision": "a6d7d8721eed59194444",
+    "url": "/static/js/1.a6d7d872.chunk.js"
+  },
+  {
+    "revision": "970aab89c3a006e0a964",
+    "url": "/static/css/main.3a8e8264.chunk.css"
+  },
+  {
+    "revision": "cc445370e9a6563b2a9cc87f7302ea1a",
+    "url": "/index.html"
+  }
+];
